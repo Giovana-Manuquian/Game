673de35ea15a2144b61ec54592295f248e9aec2c
VITOR SANTANA DE JESUS <vitor.alunos@cfp127aluno.local>
2023-10-05 15:50:13 -0300

    game

--- src/br/senai/sp/jandira/model/Battle.java
+++ src/br/senai/sp/jandira/model/Battle.java
@@ -1,5 +1,8 @@
 package br.senai.sp.jandira.model;
 
+import br.senai.sp.jandira.model.Enemy;
+import br.senai.sp.jandira.model.Player;
+
 import java.util.Scanner;
 
 public class Battle {
@@ -7,16 +10,14 @@ public class Battle {
 
     Scanner scanner = new Scanner(System.in);
 
-    public void startBattle(Player player, Enemy enemy){
-
+    public void startBattle(Player player, Enemy enemy) {
         boolean continuar = true;
 
         while (true) {
-
             int lifePlayer = player.getVida();
             int lifeEnemy = enemy.getVida();
 
-            if(lifeEnemy <= 0){
+            if (lifeEnemy <= 0) {
                 System.out.println("O player " + player.getNome() + " é o vencedor!");
                 break;
             } else if (lifePlayer <= 0) {
@@ -29,26 +30,72 @@ public class Battle {
 
             String ataque = scanner.nextLine();
 
-            if(ataque.equalsIgnoreCase("V")){
+            if (ataque.equalsIgnoreCase("V")) {
                 System.out.println("O Player atacou");
 
                 int danoPlayer = ((int) (Math.random() * 20) + 1);
 
-                enemy.subtrairVida(danoPlayer);
+
+                System.out.println("Digite um número de [1 a 5] para defesa: ");
+                int defesaEnemy = scanner.nextInt();
+                scanner.nextLine();
+
+                int defesaRandom = ((int) (Math.random() * 5) + 1);
+
+                if (defesaEnemy == defesaRandom) {
+                    System.out.println("O Enemy se defendeu com sucesso!");
+
+                    int defesaRandomEnemy = ((int) (Math.random() * 20) + 1);
+
+                    if (defesaRandomEnemy > danoPlayer) {
+                        System.out.println("Bloqueou 100%");
+                    } else {
+                        danoPlayer -= defesaRandomEnemy;
+                        enemy.subtrairVida(danoPlayer);
+                    }
+
+                } else {
+                    System.out.println("Errou a defesa ...");
+                    enemy.subtrairVida(danoPlayer);
+                }
+
                 System.out.println("O dano gerado foi de: " + danoPlayer);
 
             } else if (ataque.equalsIgnoreCase("A")) {
                 System.out.println("O Enemy atacou");
 
                 int danoEnemy = ((int) (Math.random() * 20) + 1);
-                player.subtrairVida(danoEnemy);
                 System.out.println("O dano gerado foi de: " + danoEnemy);
+
+
+                System.out.println("Digite um número de [1 a 5] para defesa: ");
+                int defesaPlayer = scanner.nextInt();
+                scanner.nextLine();
+
+                int defesaRandom = ((int) (Math.random() * 5) + 1);
+
+                if (defesaPlayer == defesaRandom) {
+                    System.out.println("O Player se defendeu com sucesso!");
+
+                    int defesaRandomPlayer = ((int) (Math.random() * 20) + 1);
+
+                    if (defesaRandomPlayer > danoEnemy) {
+                        System.out.println("Bloqueou 100%");
+                    } else {
+                        danoEnemy -= defesaRandomPlayer;
+                        player.subtrairVida(danoEnemy);
+                    }
+
+                } else {
+                    System.out.println("Errou a defesa ...");
+                    player.subtrairVida(danoEnemy);
+                }
+
+
             } else {
                 System.out.println("Digite uma tecla válida");
             }
-        }
-    }
-    public void reniciarVida(Player player, Enemy enemy){
 
+        }
     }
 }
